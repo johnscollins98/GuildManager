de9936a61d5c1f90eed1ce8dd63262ab45d24620
John Collins <johnscollins98@gmail.com>
2022-08-13 07:42:22 +0100

    rename GetGuilds to GetGuildMembers in DiscordController

--- Api/Controllers/DiscordController.cs
+++ Api/Controllers/DiscordController.cs
@@ -20,7 +20,7 @@ public class DiscordController : ControllerBase
 
   [Authorize(Policy = "AdminPolicy")]
   [HttpGet("Guilds/{guildId}/Members")]
-  public async Task<ActionResult<IEnumerable<GuildMemberDto>>> GetGuilds(string guildId)
+  public async Task<ActionResult<IEnumerable<GuildMemberDto>>> GetGuildMembers(string guildId)
   {
     var members = await discordService.GetGuildMembersAsync(guildId);
     if (members == null)
