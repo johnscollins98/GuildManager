231c2b1fb96a89816f8c10e649ffec2709eccc6c
John Collins <johnscollins98@gmail.com>
2022-08-26 21:44:07 +0100

    improve error display

--- clientapp/src/components/common/ErrorDisplay.tsx
+++ clientapp/src/components/common/ErrorDisplay.tsx
@@ -2,17 +2,21 @@ import { AxiosError } from 'axios';
 import { FC } from 'react';
 
 interface ErrorProps {
-  error: AxiosError
+  error: AxiosError;
 }
- 
+
 const ErrorDisplay: FC<ErrorProps> = ({ error }) => {
-  return (<div className='alert alert-danger'>
-    {error.code === '404' ? (
-      "Requested resource does not exist"
-    ) : (
-      "An unknown error occurred. If the issue persists please contact an administrator."
-    )}
-  </div>);
-}
- 
+  return (
+    <div className="alert alert-danger">
+      {error.response?.status === 404
+        ? 'Requested resource does not exist'
+        : error.response?.status === 401
+        ? 'You are not logged in and cannot view this content!'
+        : error.response?.status === 403
+        ? 'You do not have permissions to view this content!'
+        : 'An error occurred. If the issue persists please contact an administrator.'}
+    </div>
+  );
+};
+
 export default ErrorDisplay;
