d8c97a937693480fcb26fabde1d2aa2a02c71e32
John Collins <johnscollins98@gmail.com>
2022-08-27 18:04:07 +0100

    improve guild api resilience to not found errors

--- GuildManager.Data/Apis/Discord/GuildApi.cs
+++ GuildManager.Data/Apis/Discord/GuildApi.cs
@@ -14,27 +14,32 @@ public class GuildApi : IGuildApi
 
   public Task<Guild?> GetGuildDetailsAsync(string guildId)
   {
-    return client.GetFromJsonAsync<Guild?>($"{guildId}");
+    return fetchNullableValueAsync<Guild>($"{guildId}");
   }
 
-  public async Task<GuildMember?> GetGuildMemberAsync(string guildId, string userId)
+  public Task<GuildMember?> GetGuildMemberAsync(string guildId, string userId)
   {
-    var response = await client.GetAsync($"{guildId}/members/{userId}");
-    if (response.StatusCode == HttpStatusCode.NotFound)
-    {
-      return null;
-    }
-    response.EnsureSuccessStatusCode();
-    return await response.Content.ReadFromJsonAsync<GuildMember>();
+    return fetchNullableValueAsync<GuildMember>($"{guildId}/members/{userId}");
   }
 
   public Task<IEnumerable<GuildMember>?> GetGuildMemberListAsync(string guildId, int limit)
   {
-    return client.GetFromJsonAsync<IEnumerable<GuildMember>?>($"{guildId}/members?limit={limit}");
+    return fetchNullableValueAsync<IEnumerable<GuildMember>>($"{guildId}/members?limit={limit}");
   }
 
   public Task<IEnumerable<Role>?> GetGuildRoleListAsync(string guildId)
   {
-    return client.GetFromJsonAsync<IEnumerable<Role>?>($"{guildId}/roles");
+    return fetchNullableValueAsync<IEnumerable<Role>>($"{guildId}/roles");
+  }
+
+  private async Task<T?> fetchNullableValueAsync<T>(string endpoint)
+  {
+    var response = await client.GetAsync(endpoint);
+    if (response.StatusCode == HttpStatusCode.NotFound)
+    {
+      return default(T);
+    }
+    response.EnsureSuccessStatusCode();
+    return await response.Content.ReadFromJsonAsync<T>();
   }
 }
